e2aac004b4b7c8ab3be7c8efb94142dfaf92d5a7
Rui Peres <sauron.is.alive@gmail.com>
2017-10-21 23:17:30 +0100

    Adding into to the flow

--- TeamGen/Sources/Components/UI/Flow/Flow.swift
+++ TeamGen/Sources/Components/UI/Flow/Flow.swift
@@ -1,17 +1,47 @@
 import UIKit
 
 protocol Flow {
-    func present(_ viewController: UIViewController)
-    func dismiss()
+    func present(_ viewController: UIViewController, animated: Bool)
+    func dismiss(_ animated: Bool)
 }
 
 extension UIWindow: Flow {
-    func present(_ viewController: UIViewController) {
+    func present(_ viewController: UIViewController, animated: Bool) {
         self.rootViewController = viewController
         self.makeKeyAndVisible()
     }
 
-    func dismiss() {
+    func dismiss(animated: Bool) {
         self.rootViewController = nil
+    }
+}
+
+extension UINavigationController: Flow {
+    func present(_ viewController: UIViewController, animated: Bool) {
+        self.pushViewController(viewController, animated: animated)
+    }
+
+    func dismiss(_ animated: Bool) {
+        self.popViewController(animated: animated)
+    }
+}
+
+extension UIViewController: Flow {
+    func present(_ viewController: UIViewController, animated: Bool) {
+        self.present(viewController, animated: animated, completion: nil)
+    }
+
+    func dismiss(_ animated: Bool) {
+        self.dismiss(animated: animated, completion: nil)
+    }
+}
+
+extension UIViewController {
+    var modalFlow: Flow {
+        return self
+    }
+
+    var navigationFlow: Flow? {
+        return self.navigationController
     }
 }
