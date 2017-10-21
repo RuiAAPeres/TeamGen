ed8f24b6e4500fcaee8eed6c9440fbf78876f97b
Rui Peres <sauron.is.alive@gmail.com>
2017-10-21 21:57:34 +0100

    Dependencies

--- TeamGen/Sources/Application/AppDelegate.swift
+++ TeamGen/Sources/Application/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import ReactiveSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -6,7 +7,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 }
