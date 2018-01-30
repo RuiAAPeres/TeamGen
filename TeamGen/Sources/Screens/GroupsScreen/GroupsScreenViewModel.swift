58f50d9a2001eb66868397e2f14983c9f4021a2d
Rui Peres <sauron.is.alive@gmail.com>
2018-01-30 00:22:37 +0000

    Initial work in the state machine + Feedback loops

--- TeamGen/Sources/Screens/GroupsScreen/GroupsScreenViewModel.swift
+++ TeamGen/Sources/Screens/GroupsScreen/GroupsScreenViewModel.swift
@@ -1,30 +1,44 @@
 import ReactiveSwift
 import ReactiveFeedback
+import enum Result.NoError
 
 struct GroupsScreenViewModel: ViewLifeCycleObservable {
 
     let state: Property<State>
     let viewLifecycle: MutableProperty<ViewLifeCycle>
 
-    init(groupsBusinessController: GroupsRepositoryProtocol) {
-        state = Property(initial: .loading,
-                              reduce: GroupsScreenViewModel.reducer,
-                              feedbacks: [])
+    init(groupsReposiotry: GroupsRepositoryProtocol) {
 
-        viewLifecycle = MutableProperty(.unknown)
+        let lifeCycle = MutableProperty<ViewLifeCycle>(.unknown)
+        viewLifecycle = lifeCycle
+
+        state = Property(initial: .initial,
+                         reduce: GroupsScreenViewModel.reducer,
+                         feedbacks: [
+                            GroupsScreenViewModel.readyToLoad(lifeCycle.producer),
+                            GroupsScreenViewModel.loadGroups(groupsReposiotry)
+            ])
     }
 }
 
 extension GroupsScreenViewModel {
-    enum State {
+    enum State: Equatable {
         case initial
         case loading
-        case empty
         case loaded([Group])
+
+        static func == (lhs: State, rhs: State) -> Bool {
+            switch(lhs, rhs) {
+            case (.initial, .initial): return true
+            case (.loading, .loading): return true
+            case (let .loaded(lhsGroup), let .loaded(rhsGroup)): return lhsGroup == rhsGroup
+            default: return false
+            }
+        }
     }
 
     enum Event {
-        case startLoadingGroups
+        case viewIsReady
         case loaded([Group])
     }
 }
@@ -34,12 +48,31 @@ extension GroupsScreenViewModel {
                         event: Event)
         -> State {
             switch (state, event) {
-            case (_, .startLoadingGroups):
+            case (_, .viewIsReady):
                 return .loading
-            case (_, let .loaded(groups)) where groups.count == 0:
-                return .empty
             case (_, let .loaded(groups)):
                 return .loaded(groups)
             }
+    }
+}
+
+extension GroupsScreenViewModel {
+    static func loadGroups(_ groupsRepository: GroupsRepositoryProtocol) -> Feedback<State, Event> {
+        return Feedback { state -> SignalProducer<Event, NoError> in
+            guard state == State.loading else { return .empty }
+            return groupsRepository.groups()
+                .map(Event.loaded)
+                .flatMapError { _ in .empty }
+        }
+    }
+
+    static func readyToLoad(_ lifeCycle: SignalProducer<ViewLifeCycle, NoError>) -> Feedback<State, Event> {
+        return Feedback { state -> SignalProducer<Event, NoError> in
+            guard state == State.initial else { return .empty }
+            return lifeCycle
+                .take(first: 1)
+                .filter { $0 == .didLoad }
+                .map { _ in Event.viewIsReady }
+        }
     }
 }
